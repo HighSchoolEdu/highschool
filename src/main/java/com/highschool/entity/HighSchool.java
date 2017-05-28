ea30e7d27b66dc3fae4baea758ec58ef0ab9f9b1
tommy <tommy@jrsys.com.tw>
2017-05-28 16:10:13 +0800

    Initial commit

---
+++ src/main/java/com/highschool/entity/HighSchool.java
@@ -0,0 +1,73 @@
+package com.highschool.entity;
+import org.springframework.roo.addon.dbre.RooDbManaged;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
+import org.springframework.roo.addon.tostring.RooToString;
+
+import javax.persistence.Query;
+import java.util.List;
+
+@RooJavaBean
+@RooToString
+@RooJpaActiveRecord(versionField = "", table = "HighSchool")
+@RooDbManaged(automaticallyDelete = true)
+public class HighSchool {
+
+    public static List<String> findHighSchoolByCondition() {
+        String jpaQuery = "SELECT o.category FROM HighSchool o group by o.category";
+        return (List<String>) entityManager().createQuery(jpaQuery).getResultList();
+    }
+
+    public static List<HighSchool> findHighSchoolByCondition(String category,String obstacle) {
+        String jpaQuery = "SELECT o FROM HighSchool o where 1=1 ";
+
+        if(!"".equals(category) && category!=null) {
+            jpaQuery = jpaQuery + " and o.category=:category ";
+        }
+
+
+        if(!"".equals(obstacle) && obstacle!=null) {
+            if("earObstacle".equals(obstacle.trim())){
+                 jpaQuery = jpaQuery + " and o.earObstacle !=0 and o.earObstacle !=null ";
+            }
+        }else if(!"".equals(obstacle) && obstacle!=null) {
+            if("listenObstacle".equals(obstacle.trim())){
+                jpaQuery = jpaQuery + " and o.listenObstacle !=0 and o.listenObstacle !=null ";
+            }
+        }else if(!"".equals(obstacle) && obstacle!=null) {
+            if("lambBrainObstacle".equals(obstacle.trim())){
+                jpaQuery = jpaQuery + " and o.lambBrainObstacle !=0 and o.lambBrainObstacle !=null ";
+            }
+        }else if(!"".equals(obstacle) && obstacle!=null) {
+            if("emotionObstacle".equals(obstacle.trim())){
+                jpaQuery = jpaQuery + " and o.emotionObstacle !=0 and o.emotionObstacle !=null ";
+            }
+        }else if(!"".equals(obstacle) && obstacle!=null) {
+            if("learnObstacle".equals(obstacle.trim())){
+                jpaQuery = jpaQuery + " and o.learnObstacle !=0 and o.learnObstacle !=null ";
+            }
+        }else if(!"".equals(obstacle) && obstacle!=null) {
+            if("autism".equals(obstacle.trim())){
+                jpaQuery = jpaQuery + " and o.autism !=0 and o.autism !=null ";
+            }
+        }else if(!"".equals(obstacle) && obstacle!=null) {
+            if("otherObstacle".equals(obstacle.trim())){
+                jpaQuery = jpaQuery + " and o.otherObstacle !=0 and o.otherObstacle !=null ";
+            }
+        }else if(!"".equals(obstacle) && obstacle!=null) {
+            if("normal".equals(obstacle.trim())){
+                jpaQuery = jpaQuery + " and o.noCategory !=0 and o.noCategory !=null ";
+            }
+        }
+
+
+        Query query=entityManager().createQuery(jpaQuery);
+        if(!"".equals(category) && category!=null) {
+            query.setParameter("category", category);
+        }
+
+
+
+        return (List<HighSchool>) query.getResultList();
+    }
+}
---
+++ src/main/java/com/highschool/web/HighSchoolController.java
@@ -0,0 +1,60 @@
+package com.highschool.web;
+import com.highschool.entity.HighSchool;
+import org.springframework.roo.addon.web.mvc.controller.scaffold.RooWebScaffold;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RequestMapping("/highschools")
+@Controller
+@RooWebScaffold(path = "highschools", formBackingObject = HighSchool.class)
+public class HighSchoolController {
+
+
+    @RequestMapping(value = "/listForm", produces = "text/html")
+    public String listConditionForm(@RequestParam(value = "category", required = false) String category, @RequestParam(value = "size", required = false) Integer size, Model uiModel) {
+        List<String> listString=HighSchool.findHighSchoolByCondition();
+        for(String str:listString){
+            System.out.println("str"+str);
+        }
+        Map<String,String> stringList=new HashMap<String,String>();
+        stringList.put("earObstacle","視覺障礙");
+        stringList.put("listenObstacle","聽語障礙");
+        stringList.put("lambBrainObstacle","肢障腦麻病弱");
+        stringList.put("emotion","情緒行為障礙");
+        stringList.put("learnObstacle","學習障礙");
+        stringList.put("autism","自閉症");
+        stringList.put("otherObstacle","其他障礙");
+        stringList.put("normal","普通科");
+        uiModel.addAttribute("obstacles",stringList);
+        uiModel.addAttribute("categorys",listString);
+
+        return "highschools/listForm";
+    }
+
+
+    @RequestMapping(value = "/listResult",produces = "text/html",method = {RequestMethod.POST,RequestMethod.GET})
+    public String listCondition(@RequestParam(value = "category", required = false) String category,@RequestParam(value = "obstacle", required = false) String obstacle, Model uiModel) {
+
+
+            List<HighSchool> highSchools=HighSchool.findHighSchoolByCondition(category,obstacle);
+            for(HighSchool highSchool:highSchools){
+                System.out.println(highSchool.getAddress());
+            }
+            uiModel.addAttribute("highschools", highSchools);
+
+        return "highschools/listOb";
+    }
+
+
+}
